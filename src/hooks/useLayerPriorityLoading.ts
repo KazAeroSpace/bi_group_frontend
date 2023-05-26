02d83ff1becd24605f0045d79df8aa9bb561beaa
Alexandr Krassavin <walonade1@gmail.com>
2023-05-26 17:56:11 +0600

    removed Loading Provider

--- src/hooks/useLayerPriorityLoading.ts
+++
@@ -1,4 +0,0 @@
-import { useContext } from 'react'
-import { LayerPriorityLoadingContext, type State } from '../contexts/LayerPriorityLoading'
-
-export const useLayerPriorityLoading = (): State => useContext(LayerPriorityLoadingContext)
